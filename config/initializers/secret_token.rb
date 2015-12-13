5d18fec5fcea1beb05ae2a1b3da19f3604c5a80b
titanium-cranium <titaniumcranium@icloud.com>
2015-12-13 06:23:25 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flixter::Application.config.secret_key_base = 'fc0f03520751fb3aba8ade221001ee33cf4aaad61fb32c1c491bb9a7411192e2fbcb6ff704e56ab41143fbee42444aa5b59369b7d9193277d8063bd29be3f2a3'
