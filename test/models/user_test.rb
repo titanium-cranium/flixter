e859b71ade29f71863a2bc7e1a89f3d76eaf631f
titanium-cranium <titaniumcranium@icloud.com>
2015-12-14 10:38:46 +0000

    Added sticky footer

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "create user" do
+		user = FactoryGirl.create(:user)
+		assert_response :success
+	end
 end
